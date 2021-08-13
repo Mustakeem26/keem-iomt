bd13d92073fd44067352f27c2f0cb11c4314e37f
Econxzq <71057086+Mustakeem26@users.noreply.github.com>
2021-08-13 14:22:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <h1>Mustakeem Laehlong 62107677</h1>
 
-<iframe src="https://thingspeak.com/channels/1458411/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="100%" higth="100%"></iframe>
+<iframe src="https://thingspeak.com/channels/1458411/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="50" higth="50"></iframe>
+<iframe src="https://thingspeak.com/channels/1458411/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="50" higth="50"></iframe>
+<iframe src="https://thingspeak.com/channels/1458411/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="50" higth="50"></iframe>
