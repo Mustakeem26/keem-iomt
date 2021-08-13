55c509d7192e583ad6e5a0fe10a66ec46bb119fd
Econxzq <71057086+Mustakeem26@users.noreply.github.com>
2021-08-13 14:33:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <h1>Mustakeem Laehlong 62107677</h1>
 
-<iframe src="https://thingspeak.com/channels/1458411/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:300px;width:500px;" title="HUMIDITY"></iframe>
+<iframe src="https://thingspeak.com/channels/1458411/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:200px;width:500px;" title="HUMIDITY"></iframe>
 <br>
-<iframe src="https://thingspeak.com/channels/1458411/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:300px;width:500px;" title="TEMPERATURE"></iframe>
+<iframe src="https://thingspeak.com/channels/1458411/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:200px;width:500px;" title="TEMPERATURE"></iframe>
 <br>
-<iframe src="https://thingspeak.com/channels/1458411/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:300px;width:500px;" title="LIGHT"></iframe>
+<iframe src="https://thingspeak.com/channels/1458411/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:200px;width:500px;" title="LIGHT"></iframe>
