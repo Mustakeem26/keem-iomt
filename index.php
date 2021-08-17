6e448df923f1d0b385f7f36267502378e60a34b6
Econxzq <71057086+Mustakeem26@users.noreply.github.com>
2021-08-17 21:20:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,39 @@
-<h1>Mustakeem Laehlong 62107677</h1>
+<html>
+<head>
+<script>
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.thingspeak.com/channels/1458411/fields/1.json? 
+api_key=RIAQ3PEGDT6ZR84N&results=1', true);
 
-<iframe src="https://thingspeak.com/channels/1458411/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="HUMIDITY"></iframe>
-var urlOption = 'https://api.thingspeak.com/channels/1458411/fields/1.json?results=1';
-var dataOption = {
-results: numPointsToPlot,
-start: startDate,
-end: endDate
-};
-<br>
-<iframe src="https://thingspeak.com/channels/1458411/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="TEMPERATURE"></iframe>
-<br>
-<iframe src="https://thingspeak.com/channels/1458411/maps/channel_show" style="height:250px;width:500px;" title="LIGHT"></iframe>
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400) {
+    var data = JSON.parse(request.responseText);
+    var dia = date.getDate();
+    var mes = date.getMonth();
+    mes++;
+    var ano = date.getFullYear();
+    var hora = date.getHours();
+    var minuto = date.getMinutes();
+    document.getElementById("camb").innerHTML = "Câmbio Dólar: R$ " + data.feeds[0].field1 +  " | 
+    Atualizado em " + dia + "/" + mes + "/" + ano + " às " + hora + ":" + minuto;
+    } else {
+    // We reached our target server, but it returned an error
+     }
+     };
+
+    request.send();
+    </script>
+    </head>
+    <body>
+      <h1>Mustakeem Laehlong 62107677</h1>
+      <iframe src="https://thingspeak.com/channels/1458411/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="HUMIDITY"></iframe>
+      <br>
+      <iframe src="https://thingspeak.com/channels/1458411/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="TEMPERATURE"></iframe>
+      <br>
+      <iframe src="https://thingspeak.com/channels/1458411/maps/channel_show" style="height:250px;width:500px;" title="LIGHT"></iframe>
+      <div  width = "100%" id="camb" style="font-size:15px; text-align:left; color: white; margin- 
+     left: -300px; background-color: red; border-left: 300px solid red; border-bottom: 5px solid red; 
+     border-top: 300px solid red; overflow: hidden;  margin-top: -300px; font-family: Brandon, 
+     Grotesque, sans-serif;"></div>
+  </body>
+</html>
