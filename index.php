eeb95c99c7a523fc1a4e254c11bc1e61a015e9d0
Econxzq <71057086+Mustakeem26@users.noreply.github.com>
2021-08-17 22:21:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
  <body>
       <h1>Mustakeem Laehlong 62107677</h1>
       <iframe src="https://thingspeak.com/channels/1458411/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="HUMIDITY"></iframe>
-      <span id="test">20</span>
+      <span id="test"></span>
 
         <script type="text/javascript">
         $.getJSON("https://api.thingspeak.com/channels/1458411/fields/1.json?results=1", function(json) {
             // ตัวแปร json เป็น object อยู่แล้ว จะใช้ค่าอะไรก็เรียกมาใช้แบบ object ได้เลย เช่น ต้องการดึงค่า "thing" ที่อยู่ใน "with" ออกมาใช้
             var field1 = json.feeds.field1;
             // แสดงค่า thing ใน <span>
-            document.getElementById("test").innerHTML = thing;
+            document.getElementById("test").innerHTML = field1;
         });
     </script>
       <br>
