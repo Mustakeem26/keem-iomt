65df0b2e73745d0947ad55524bb19a223377c919
Econxzq <71057086+Mustakeem26@users.noreply.github.com>
2021-08-17 20:44:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,4 @@
 <br>
 <iframe src="https://thingspeak.com/channels/1458411/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="TEMPERATURE"></iframe>
 <br>
-<iframe src="https://thingspeak.com/channels/1458411/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="LIGHT"></iframe>
+<iframe src="https://thingspeak.com/channels/1458411/maps/channel_show" style="height:250px;width:500px;" title="LIGHT"></iframe>
