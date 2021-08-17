48ccfa14b11e6588a2fdb8f63603e877ffed4949
Econxzq <71057086+Mustakeem26@users.noreply.github.com>
2021-08-17 22:43:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,21 @@
 <html>
 <head>
- <script>
-async function getDataFromAPI() {
-// คา สั่งส่ง request
-let response = await fetch('https://api.thingspeak.com/channels/1458411/fields/1.json?results=1')
-let rawData = await response.text() // รับผลลัพธ์
-let data = JSON.parse(rawData) // แปลงผลลัพธ์เป็ น object
-console.log(rawData) // ทดสอบแสดงค่า
-//data.forEach(temp => { // วนลูปดึงค่าจาก object แสดงผลใน console
-//console.log(temp.feeds.field1)
-//});
-}
-</script>
+ <title></title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
  <body>
       <h1>Mustakeem Laehlong 62107677</h1>
       <iframe src="https://thingspeak.com/channels/1458411/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="HUMIDITY"></iframe>
       <span id="test"></span>
-      <script>document.getDataFromAPI("test").innerHTML = field1;</script>
+        <script type="text/javascript">
+        $.getJSON("https://api.thingspeak.com/channels/1458411/fields/1.json?results=1", function(json) {
+             //ตัวแปร json เป็น object อยู่แล้ว จะใช้ค่าอะไรก็เรียกมาใช้แบบ object ได้เลย เช่น ต้องการดึงค่า "thing" ที่อยู่ใน "with" ออกมาใช้
+            var field1 = json.feeds.field1;
+           //แสดงค่า thing ใน <span>
+            document.getElementById("test").innerHTML = field1;
+        });
+    </script>
+
       <br>
       <iframe src="https://thingspeak.com/channels/1458411/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="TEMPERATURE"></iframe>
       <br>
