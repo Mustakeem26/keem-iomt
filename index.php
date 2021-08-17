98a5b768f0ddfb8cc4a66a7edc1beb11f3977045
Econxzq <71057086+Mustakeem26@users.noreply.github.com>
2021-08-17 22:12:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,19 @@
 <html>
 <head>
-<script>
-var request = new XMLHttpRequest();
-request.open('GET', 'https://api.thingspeak.com/channels/1458411/fields/1.json? 
-api_key=RIAQ3PEGDT6ZR84N&results=1', true);
-
-request.onload = function() {
-  if (request.status >= 200 && request.status < 400) {
-    var data = JSON.parse(request.responseText);
-    var dia = date.getDate();
-    var mes = date.getMonth();
-    mes++;
-    var ano = date.getFullYear();
-    var hora = date.getHours();
-    var minuto = date.getMinutes();
-    document.getElementById("camb").innerHTML = "Câmbio Dólar: R$ " + data.feeds[0].field1 +  " | 
-    Atualizado em " + dia + "/" + mes + "/" + ano + " às " + hora + ":" + minuto;
-    } else {
-     }
-     };
-
-    request.send();
- </script>
- </head>
+</head>
  <body>
       <h1>Mustakeem Laehlong 62107677</h1>
       <iframe src="https://thingspeak.com/channels/1458411/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="HUMIDITY"></iframe>
-      <h2 id="camb">KEEM</h2>
+      <span id="test"></span>
+
+        <script type="text/javascript">
+        $.getJSON("https://api.thingspeak.com/channels/1458411/fields/1.json?results=1", function(json) {
+            // ตัวแปร json เป็น object อยู่แล้ว จะใช้ค่าอะไรก็เรียกมาใช้แบบ object ได้เลย เช่น ต้องการดึงค่า "thing" ที่อยู่ใน "with" ออกมาใช้
+            var field1 = json.feeds.field1;
+            // แสดงค่า thing ใน <span>
+            document.getElementById("test").innerHTML = thing;
+        });
+    </script>
       <br>
       <iframe src="https://thingspeak.com/channels/1458411/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="TEMPERATURE"></iframe>
       <br>
