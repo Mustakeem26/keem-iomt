292037a6e596af3ad321f20171018ac73a68f038
Econxzq <71057086+Mustakeem26@users.noreply.github.com>
2021-08-17 21:31:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ request.onload = function() {
  <body>
       <h1>Mustakeem Laehlong 62107677</h1>
       <iframe src="https://thingspeak.com/channels/1458411/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="HUMIDITY"></iframe>
-   <div id="camb" ><h2></h2></div>
+   <div id="camb" ><h2></h2>KEEM</div>
       <br>
       <iframe src="https://thingspeak.com/channels/1458411/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="TEMPERATURE"></iframe>
       <br>
