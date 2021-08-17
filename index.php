41c1010452f94aefb7d3e2d9e83cd721f37eb15d
Econxzq <71057086+Mustakeem26@users.noreply.github.com>
2021-08-17 21:09:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <h1>Mustakeem Laehlong 62107677</h1>
 
 <iframe src="https://thingspeak.com/channels/1458411/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="HUMIDITY"></iframe>
-<iframe src="https://api.thingspeak.com/channels/1458411/fields/1.json?results=1"></iframe>
+var urlOption = 'https://api.thingspeak.com/channels/1458411/fields/1.json?results=1';
+var dataOption = {
+results: numPointsToPlot,
+start: startDate,
+end: endDate
+};
 <br>
 <iframe src="https://thingspeak.com/channels/1458411/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="TEMPERATURE"></iframe>
 <br>
