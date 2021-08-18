37cc3ed9fb126461dfaf41f0d98aace741ce3922
Econxzq <71057086+Mustakeem26@users.noreply.github.com>
2021-08-18 15:13:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,17 +6,30 @@
  <body>
       <h1>Mustakeem Laehlong 62107677</h1>
       <iframe src="https://thingspeak.com/channels/1458411/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="HUMIDITY"></iframe>
-      <span id="test"></span>
-        <script type="text/javascript">
-        $.getJSON("https://api.thingspeak.com/channels/1458411/fields/1.json?results=1", function(json) {
-             //ตัวแปร json เป็น object อยู่แล้ว จะใช้ค่าอะไรก็เรียกมาใช้แบบ object ได้เลย เช่น ต้องการดึงค่า "thing" ที่อยู่ใน "with" ออกมาใช้
-            var field1 = json.feeds.field1;
-            alert(field1);
-           //แสดงค่า thing ใน <span>
-            document.getElementById("test").innerHTML = field1;
-        });
-    </script>
+          <?php
+$api_url = 'https://api.thingspeak.com/channels/1458411/fields/1.json?results=1';
 
+// Read JSON file
+$json_data = file_get_contents($api_url);
+
+// Decode JSON data into PHP array
+$response_data = json_decode($json_data);
+
+// All user data exists in 'data' object
+$user_data = $response_data->feeds;
+
+// Cut long data into small & select only first 10 records
+$user_data = array_slice($user_data, 0);
+
+// Print data if need to debug
+//print_r($user_data);
+
+// Traverse array and display user data
+foreach ($user_data as $user) {
+	echo "name: ".$user->field1;
+}
+
+?>
       <br>
       <iframe src="https://thingspeak.com/channels/1458411/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="TEMPERATURE"></iframe>
       <br>
