9151ba396475274680dcf1c6f96043333b566139
Econxzq <71057086+Mustakeem26@users.noreply.github.com>
2021-08-18 15:16:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,31 +7,27 @@
       <h1>Mustakeem Laehlong 62107677</h1>
       <iframe src="https://thingspeak.com/channels/1458411/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="HUMIDITY"></iframe>
           <?php
-$api_url = 'https://api.thingspeak.com/channels/1458411/fields/1.json?results=1';
-
-// Read JSON file
-$json_data = file_get_contents($api_url);
-
-// Decode JSON data into PHP array
-$response_data = json_decode($json_data);
-
-// All user data exists in 'data' object
-$user_data = $response_data->feeds;
-
-// Cut long data into small & select only first 10 records
-$user_data = array_slice($user_data, 0);
-
-// Print data if need to debug
-//print_r($user_data);
-
-// Traverse array and display user data
-foreach ($user_data as $user) {
-	echo "name: ".$user->field1;
-}
-
-?>
+		$api_url = 'https://api.thingspeak.com/channels/1458411/fields/1.json?results=1';
+		$json_data = file_get_contents($api_url);
+		$response_data = json_decode($json_data);
+		$user_data = $response_data->feeds;
+		$user_data = array_slice($user_data, 0);
+		foreach ($user_data as $user) {
+			echo "Humidity: ".$user->field1;
+		}
+		?>
       <br>
       <iframe src="https://thingspeak.com/channels/1458411/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="TEMPERATURE"></iframe>
+	  <?php
+		$api_url = 'https://api.thingspeak.com/channels/1458411/fields/2.json?results=1';
+		$json_data = file_get_contents($api_url);
+		$response_data = json_decode($json_data);
+		$user_data = $response_data->feeds;
+		$user_data = array_slice($user_data, 0);
+		foreach ($user_data as $user) {
+			echo "Temperature: ".$user->field2;
+		}
+		?>
       <br>
       <iframe src="https://thingspeak.com/channels/1458411/maps/channel_show" style="height:250px;width:500px;" title="LIGHT"></iframe>
 </body>
