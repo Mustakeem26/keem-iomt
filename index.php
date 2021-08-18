dc7e17dd888d976ed00e5c4772bc53f66fd40a8f
Econxzq <71057086+Mustakeem26@users.noreply.github.com>
2021-08-18 16:13:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  <body>
       <h1>Mustakeem Laehlong 62107677</h1>
       <iframe src="https://thingspeak.com/channels/1458411/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" style="height:250px;width:500px;" title="HUMIDITY"></iframe>
-         <p style="font-size:50px;"> 
+         <p style="font-size:50px;font:bold;"> 
 	 <?php
 		$api_url = 'https://api.thingspeak.com/channels/1458411/fields/1.json?results=1';
 		$json_data = file_get_contents($api_url);
